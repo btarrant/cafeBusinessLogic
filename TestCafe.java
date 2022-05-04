fe2ffa653f53bf4aad03bc7d00554d383df77bf3
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-04 16:03:16 -0400

    Tested the getOrderTotal class.

--- TestCafe.java
+++ TestCafe.java
@@ -16,9 +16,9 @@ public class TestCafe {
         System.out.println("\n----- Streak Goal Test -----");
         System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
     
-        // System.out.println("----- Order Total Test-----");
-        // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
-        // System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
         
         // System.out.println("----- Display Menu Test-----");
         
